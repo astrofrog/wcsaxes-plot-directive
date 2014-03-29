c0803f49c958c77b01b7f42383e246213fb82af0
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-03-29 23:59:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+About
+=====
+
+A standalone package of the latest developer version of plot_directive 
+from matplotlib, including some unmerged patches. This is intended for 
+use by the WCSAxes documentation.
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='plot_directive',
+      version='0.0.0',
+      packages=['plot_directive'],
+      provides=['plot_directive'],
+      requires=['matplotlib'],
+      )
